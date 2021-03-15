93dba6543eff521f595f74b94e9d45419355f321
Rae Abunahla <raghebabn@gmail.com>
2021-03-15 17:19:34 -0400

    Temprature converter implemented via React

--- src/index.js
+++ src/index.js
@@ -9,29 +9,78 @@ import './index.css';
  *  Have the radio button influence the state of the Converter class and change a boolean that we pass a prop to the convert function.
  */
 
-function Convert(props) {
-    
-}
-
 class Converter extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            toUnit: "Celsius",
+            value: '',
+            result: 0.0
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onRadioChange = this.onRadioChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    onRadioChange(event) {
+        this.setState({
+            toUnit: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        const unit = this.state.toUnit;
+        const temp = this.state.value;
+        if (unit === "Celsius") {
+            this.setState({
+                result: temp*(9/5) + 32
+            });
+        } else {
+            this.setState({
+                result: (temp-32) * (5/9)
+            });
+        }
+        event.preventDefault();
     }
 
     render() {
         return(
-            <form class="converterForm">
-            <label>
-                <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Convert" />
+            <form class="converterForm" onSubmit={this.handleSubmit}>
+                <label>
+                    Degrees:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label> 
+                <br/>
+                <label for="celsius">to Celsius</label>
+                <input type="radio" id="celsius" name="type" value="Celsius" 
+                checked={this.state.toUnit === "Celsius"} 
+                onChange={this.onRadioChange} />
+
+                <label for="farenheit">to Farenheit</label>
+                <input type="radio" id="farenheit" name="type" value="Farenheit" 
+                checked={this.state.toUnit === "Farenheit"} 
+                onChange={this.onRadioChange} />
+
+                <p>{this.state.result}</p> 
+
+                <input type="submit" value="Convert"/>
+                
             </form>
         );
     }
 }
 
 ReactDOM.render(
-    <Converter/>,
+    <div class="screen">
+        <h1 class="title"> Temperature Converter</h1>
+        <Converter/>
+    </div>,
     document.getElementById('root')
 );
